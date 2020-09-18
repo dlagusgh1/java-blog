c132223f324a93b96a520c4b2ed4acce5d17f1c4
dlagusgh1 <dlagusgh1@naver.com>
2020-09-18 09:07:19 +0900

    로고 관련 config 추가

--- src/com/sbs/java/blog/config/Config.java
+++ src/com/sbs/java/blog/config/Config.java
@@ -8,4 +8,5 @@ public class Config {
 	public static String getSiteName() {
 		return "현호블로그";
 	}
+	public static String getSiteLogo = "https://user-images.githubusercontent.com/60770834/93438490-58050500-f908-11ea-8a76-e42ba5a869ed.jpg";
 }
